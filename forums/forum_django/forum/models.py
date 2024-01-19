f8ed289b925a7ebc40cf6ae77b8b65ea73c67d84
jbillaba <jeremyvillalva66@gmail.com>
2024-01-19 13:38:13 -0500

    updated serializer to use naturaltime on created_on

--- forums/forum_django/forum/models.py
+++ forums/forum_django/forum/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timesince
 
 class User(AbstractUser):
     username=models.CharField(max_length=30, unique=True)
@@ -15,6 +16,10 @@ class Post(models.Model):
     text=models.TextField(max_length=255)
     op=models.ForeignKey(User, on_delete=models.CASCADE, related_name='originalposter')
     created_on=models.DateTimeField(auto_now_add=True)
+
+    @property
+    def timesince(self):
+        return timesince.timesince(self.created_on)
 
     def __str__(self):
         return self.title
--- forums/forum_django/forum/serializers.py
+++ forums/forum_django/forum/serializers.py
@@ -2,6 +2,7 @@ from .models import User, Post
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 ## TODO: finish this file 
 
@@ -39,8 +40,10 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model=User
         fields=['url', 'id', 'username', 'name', 'email', 'password']
 
+
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     op=serializers.SerializerMethodField("get_op")
+    created_on=serializers.SerializerMethodField("get_timesince")
     class Meta:
         model=Post
         fields=['url', 'id', 'title', 'text', 'op', 'created_on']
@@ -48,13 +51,14 @@ class PostSerializer(serializers.HyperlinkedModelSerializer):
     def get_op(self, object):
         return object.op.username
     
+    def get_timesince(self, object):
+        return naturaltime(object.created_on)
+    
     def create(self, validated_data):
         validated_data['op']=self.context['request'].user
         return super(PostSerializer, self).create(validated_data)
     
     
-
-
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, User):
